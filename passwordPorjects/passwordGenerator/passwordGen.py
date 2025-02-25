59c2de938c02293c0708a6a27fc644310c211422
bcastan24 <messi6509@gmail.com>
2025-02-24 20:09:01 -0600

    added lists of all letters, numbers and specials characters

--- passwordPorjects/passwordGenerator/passwordGen.py
+++ passwordPorjects/passwordGenerator/passwordGen.py
@@ -1,1 +1,8 @@
 #A program to generate passwords
+#It will generate a password that is at least 8 characters long, has at least: one upper case letter, one lower case letter, one number, and one special character
+import random
+#need to make lists of all upper case letters, all lower case letters, all numbers, and all special characters
+upperCaseList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z']
+lowerCaseList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z']
+numbersList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+specialCharList = ['!', '&', '*']
