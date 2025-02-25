84803a49e8ddc18c32d6b91ed19ec9ce0f6514cc
bcastan24 <messi6509@gmail.com>
2025-02-24 19:09:38 -0600

    added the pdf reader package and make the text to speech read page 8

--- pdfToAudiobook/pdfToAudiob.py
+++ pdfToAudiobook/pdfToAudiob.py
@@ -1,6 +1,13 @@
 #A program to turn a pdf into an audiobook
 #need to install pyttsx3 python package
+#also need to install pypdf2 python package
 import pyttsx3
+import PyPDF2
+book = open('oopbook.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
 speaker = pyttsx3.init()
-speaker.say("Hello World")
+page = pdfReader.getPage(7)
+text = page.extractText()
+speaker.say(text)
 speaker.runAndWait()
