766338f18ab858213fd228537ef437603913b2da
bcastan24 <messi6509@gmail.com>
2025-02-25 22:56:13 -0600

    wrote the code to crack a password by brute force

--- passwordPorjects/passwordCracker/passwordCrack.py
+++ passwordPorjects/passwordCracker/passwordCrack.py
@@ -1,1 +1,23 @@
 #A program to crack a password
+#note that this password cracker just brute forces passwords
+#for any password between 8 and 15 characters this will take a VERY long time
+import time
+
+password = input("Please enter the password you would like to test: ")
+start = time.time()
+#All possible characters that could be in password
+possibleCharsList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!&*'
+
+guess = []
+for val in range(8, 15):
+    a = [i for i in possibleCharsList]
+    for y in range(val):
+        a = [x+i for i in possibleCharsList for x in a]
+    guess = guess+a
+    if password in guess:
+        break
+end = time.time()
+clock = str(end-start)
+
+print("Your password: " + password)
+print("Time taken: " + clock)
