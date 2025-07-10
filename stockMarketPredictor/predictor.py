2e6d9b82ebafad7a87499fa545957a07fdc51c3e
bcastan24 <messi6509@gmail.com>
2025-07-09 21:21:02 -0500

    imported all the necessary libraries

--- stockMarketPredictor/predictor.py
+++ stockMarketPredictor/predictor.py
@@ -1,1 +1,12 @@
+#A program that uses machine learning to predict the stock market
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import urllib.request, json
+import datetime as dt
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+
 
