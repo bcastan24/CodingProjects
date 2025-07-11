2111d0a650124d763c34d5e7c1a7a24efde3d487
bcastan24 <messi6509@gmail.com>
2025-07-10 22:11:02 -0500

    wrote code to clean data

--- basicMachineLearning/predictingBaseballStats.py
+++ basicMachineLearning/predictingBaseballStats.py
@@ -6,12 +6,39 @@ from pybaseball import batting_stats
 
 #defining the start and end year that we want to pull data from
 START = 2002
-END = 2022
+END = 2004
 
 #downloading the data
 batting = batting_stats(START, END, qual=200)
 batting.to_csv("batting.csv")
 #splitting data into groups based on player id and removing players that we only have one season of data on
-batting = batting.groupby("IDfg", group_keys=False).filter(lambda x: x.shape[0] > 1)
+batting = batting.groupby("IDfg").filter(lambda x: x.shape[0] > 1)
+
+#Machine Learning target
+def nextSeason(player):
+    player = player.sort_values("Season")
+    player["Next_WAR"] = player["WAR"].shift(-1)
+    return player
+
+batting = batting.groupby("IDfg").apply(nextSeason, include_groups=False)
+
+#Cleaning Data
+#Counting all the columns that are empty
+nullCount = batting.isnull().sum()
+#selecting all the columns that are complete
+completeCols = list(batting.columns[nullCount == 0])
+#Removing all of the null columns
+batting = batting[completeCols + ["Next_WAR"]].copy()
+#Because our machine learning only takes numbers we need to get rid of the columns that are strings or turn them into numbers
+#Deleting columns that are strings that we don't need
+del batting["Dol"]
+del batting["Age Rng"]
+#turning team name into a number
+batting["team_code"] = batting["Team"].astype("category").cat.codes
+#making a copy of the data
+batting_full = batting.copy()
+batting = batting.dropna()
+
+
 
 
