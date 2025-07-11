649dc4392f63905bb0de956df23488c4bf6aa647
bcastan24 <messi6509@gmail.com>
2025-07-10 22:50:27 -0500

    wrote code to select useful features

--- basicMachineLearning/predictingBaseballStats.py
+++ basicMachineLearning/predictingBaseballStats.py
@@ -3,6 +3,10 @@ import os
 import pandas as pd
 import numpy as np
 from pybaseball import batting_stats
+from sklearn.linear_model import Ridge
+from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.preprocessing import MinMaxScaler
 
 #defining the start and end year that we want to pull data from
 START = 2002
@@ -12,7 +16,7 @@ END = 2004
 batting = batting_stats(START, END, qual=200)
 batting.to_csv("batting.csv")
 #splitting data into groups based on player id and removing players that we only have one season of data on
-batting = batting.groupby("IDfg").filter(lambda x: x.shape[0] > 1)
+batting = batting.groupby("IDfg", as_index=False).filter(lambda x: x.shape[0] > 1)
 
 #Machine Learning target
 def nextSeason(player):
@@ -37,8 +41,21 @@ del batting["Age Rng"]
 batting["team_code"] = batting["Team"].astype("category").cat.codes
 #making a copy of the data
 batting_full = batting.copy()
-batting = batting.dropna()
+batting = batting.dropna().copy()
 
-
+#selecting useful features
+rr = Ridge(alpha=1)
+#split data into 3 parts but make sure that ML is not using data from the future to predict WARs of the past
+split = TimeSeriesSplit(n_splits=3)
+sfs = SequentialFeatureSelector(rr, n_features_to_select=20, direction="forward", cv=split, n_jobs=4)
+#taking out columns that we don't need to run through ML
+removedColumns = ["Next_WAR", "Name", "Team", "IDfg", "Season"]
+#take all the columns that are not in removedColumns
+selectedColumns = batting.columns[~batting.columns.isin(removedColumns)]
+scaler = MinMaxScaler()
+#batting.loc[:, selectedColumns] = scaler.fit_transform(batting[selectedColumns])
+batting[selectedColumns] = scaler.fit_transform(batting[selectedColumns].astype("float32"))
+sfs.fit(batting[selectedColumns], batting["Next_WAR"])
+predictors = list(selectedColumns[sfs.get_support()])
 
 
