a9be6756b669e8fb5b843957e0321c42b4d65237
bcastan24 <messi6509@gmail.com>
2025-07-10 21:28:20 -0500

    wrote code to download batting data

--- basicMachineLearning/predictingBaseballStats.py
+++ basicMachineLearning/predictingBaseballStats.py
@@ -1,1 +1,17 @@
+#An algorithm to predict a baseball player's WAR
+import os
+import pandas as pd
+import numpy as np
+from pybaseball import batting_stats
+
+#defining the start and end year that we want to pull data from
+START = 2002
+END = 2022
+
+#downloading the data
+batting = batting_stats(START, END, qual=200)
+batting.to_csv("batting.csv")
+#splitting data into groups based on player id and removing players that we only have one season of data on
+batting = batting.groupby("IDfg", group_keys=False).filter(lambda x: x.shape[0] > 1)
+
 
