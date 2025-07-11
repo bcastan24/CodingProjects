5e87c14abe12bf7ce76eef776b84dd06ef433faf
bcastan24 <messi6509@gmail.com>
2025-07-11 16:02:27 -0500

    added another neuron and the feedForward function

--- basicMachineLearning/predictingStatsUsingNN.py
+++ basicMachineLearning/predictingStatsUsingNN.py
@@ -8,8 +8,8 @@ each player
 I am going to feed the neural network WAR, OPS, BABIP, and wRC+ over four years and use that to make a prediction of WAR
 for the next season
 
-I am only going to use 2 neurons in the hidden layer because this is a simple NN, and with the two hidden layer neurons
-and 1 output neuron I am at 48 weights
+I am only going to use 3 neurons in the hidden layer because this is a simple NN, and with the two hidden layer neurons
+and 1 output neuron I am at 50 weights
 
 I do realize this is non-optimal, but I am still very new to NN, so I want to do a fun project to learn a bit more
 '''
@@ -38,7 +38,7 @@ class Neuron:
         self.bias = bias
 
     def feedForward(self, inputs):
-        #does th dot product of the array of weights and array of inputs; ((w1*x1)+(w2*x2))+b
+        #does the dot product of the array of weights and array of inputs; ((w1*x1)+(w2*x2))+b
         total = np.dot(self.weights, inputs) + self.bias
         #then put that dot product into the sigmoid activation function to get the feedforward
         return sigmoid(total)
@@ -81,7 +81,7 @@ class OurNeuralNetwork:
         self.w30 = np.random.normal()
         self.w31 = np.random.normal()
         self.w32 = np.random.normal()
-        #o1 weights
+        #h3 weights
         self.w33 = np.random.normal()
         self.w34 = np.random.normal()
         self.w35 = np.random.normal()
@@ -98,12 +98,27 @@ class OurNeuralNetwork:
         self.w46 = np.random.normal()
         self.w47 = np.random.normal()
         self.w48 = np.random.normal()
+        #o1 weights
+        self.w49 = np.random.normal()
+        self.w50 = np.random.normal()
+        self.w51 = np.random.normal()
 
 
         #biases
         self.b1 = np.random.normal()
         self.b2 = np.random.normal()
         self.b3 = np.random.normal()
+        self.b4 = np.random.normal()
 
         self.wh1 = np.array([self.w1, self.w2, self.w3, self.w4, self.w5, self.w6, self.w7, self.w8, self.w9, self.w10, self.w11, self.w12, self.w13, self.w14, self.w15, self.w16])
         self.wh2 = np.array([self.w17, self.w18, self.w19, self.w20, self.w21, self.w22, self.w23, self.w24, self.w25, self.w26, self.w27, self.w28, self.w29, self.w30, self.w31, self.w32])
+        self.wh3 = np.array([self.w33, self.w34, self.w35, self.w36, self.w37, self.w38, self.w39, self.w40, self.w41, self.w42, self.w43, self.w44, self.w45, self.w46, self.w47, self.w48])
+
+        def feedForward(self, x):
+            # where x is a numpy array with 2 elements
+
+            h1 = sigmoid(np.dot(self.wh1, x) + self.b1)
+            h2 = sigmoid(np.dot(self.wh2, x) + self.b2)
+            h3 = sigmoid(np.dot(self.wh3, x) + self.b3)
+            o1 = sigmoid(self.w49 * h1 + self.w50 * h2 + self.w51 * h3 + self.b4)
+            return o1
