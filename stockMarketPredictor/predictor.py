051926cd9c4c5c2d59f0116aa6a5f004bf8d32c9
bcastan24 <messi6509@gmail.com>
2025-07-15 11:06:49 -0500

    wrote code to plot data and fixed data retriving error

--- stockMarketPredictor/predictor.py
+++ stockMarketPredictor/predictor.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import MinMaxScaler
 dataSource = 'alphavantage'
 
 if dataSource == 'alphavantage':
-    apikey = "0VCUU9FT5X9BDFC2"
+    apikey = '0VCUU9FT5X9BDFC2'
     ticker = "AAL"
     #getting stock market data from AAL from the last 20 years
     urlString = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(ticker,apikey)
@@ -25,11 +25,11 @@ if dataSource == 'alphavantage':
             data = json.loads(url.read().decode())
             #extract the stock market data that we want
             data = data['Time Series (Daily)']
-            df = pd.DataFrame(columns=['Date', 'Low', 'High', 'Close', 'Open'])
+            df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
             for k, v in data.items():
                 date = dt.datetime.strptime(k, '%Y-%m-%d')
-                dataRow = [date.date(), float(v['3. low']), float(v['2. high']), float(v['4. close']), float(v['1. open'])]
-                df.loc[-1:] = dataRow
+                dataRow = [date.date(),float(v['3. low']),float(v['2. high']),float(v['4. close']),float(v['1. open'])]
+                df.loc[-1,:] = dataRow
                 df.index = df.index + 1
         print("Data saved to: %s"%fileToSave)
         df.to_csv(fileToSave)
@@ -37,4 +37,18 @@ if dataSource == 'alphavantage':
     else:
         print("Loading Data from CSV")
         df = pd.read_csv(fileToSave)
+
+#sort data by date
+df = df.sort_values('Date')
+#double check the result
+df.head()
+
+#plotting data
+plt.figure(figsize=(18,9))
+plt.plot(range(df.shape[0]), (df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
+
 
