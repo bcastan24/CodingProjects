b174d8e2c6e0a5f86c5e018948a9f497689b7d9e
bcastan24 <messi6509@gmail.com>
2025-07-16 16:56:05 -0500

    added comments and added two files for linkedlists

--- pythonToGoogleSheets/internshipInfo/mainPTS.py
+++ pythonToGoogleSheets/internshipInfo/mainPTS.py
@@ -1,3 +1,7 @@
+'''Yes I do realize that this is really over engineering to just add internship oppotunities and rank them in a
+ Google Sheet, but this is good practice with data structures like linked lists, manipulating data, and
+ using the Google API'''
+
 import googleTools as gt
 
 gt.updateFile()
---
+++ pythonToGoogleSheets/internshipInfo/node.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, company:str, loc:str, title:str, length:str, start:str, pay:str, requiredSkills:str, link:str):
+        self.company = company
+        self.loc = loc
+        self.title = title
+        self.length = length
+        self.start = start
+        self.pay = pay
+        self.requiredSkills = requiredSkills
+        self.link = link
+        self.next = None
+
+
