03ea0cc2d1ef8f276487f0b06eb015fa8bab0a18
bcastan24 <messi6509@gmail.com>
2025-07-16 16:01:34 -0500

    wrote code to authenticate user and pull prefered file and download it

--- pythonToGoogleSheets/internshipInfo/intershipInfo.py
+++ pythonToGoogleSheets/internshipInfo/intershipInfo.py
@@ -1,2 +1,17 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+
+
+gauth = GoogleAuth()
+#creates local webserver and auto handles authentication
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+def updateFile() -> None:
+    fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    for file1 in fileList:
+        if file1['id'] == "12ChYdknEK6Mfw9gfK2IYG4RxONdf35BpIHBunOv0Too":
+            file2 = file1
+
+    filetoCSV = drive.CreateFile({'id': file2['id']})
+    filetoCSV.GetContentFile('file.csv', mimetype='text/csv')
