d1e3dc05678ba5fa1477b7223ce72c59d487303d
bcastan24 <messi6509@gmail.com>
2025-07-21 12:00:29 -0500

    fixed spelling error

--- pythonToGoogleSheets/internshipInfo/mainPTS.py
+++ pythonToGoogleSheets/internshipInfo/mainPTS.py
@@ -1,4 +1,4 @@
-'''Yes I do realize that this is really over engineering to just add internship oppotunities and rank them in a
+'''Yes I do realize that this is really over engineering to just add internship opportunities and rank them in a
  Google Sheet, but this is good practice with data structures like linked lists, manipulating data, and
  using the Google API. This also helps practice principle of least privilege and reading documentation, for all the code
  for the Google API I read the documentation to figure what to write and why. I left out some key files that
